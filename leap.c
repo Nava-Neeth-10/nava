92592890f8c557d4d2f850319e61e2e80dd09f3b
Nava-Neeth-10 <47468411+Nava-Neeth-10@users.noreply.github.com>
2019-02-09 16:15:49 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+    int a;
+
+    scanf("%d",&a);
+
+    
+    if(a%4==0){
+
+    		printf("yes");
+
+    	}
+
+    	else {
+    		printf("no");
+
+    	}
+      return 0;
+}
