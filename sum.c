5f98497001cfc4ac8dd277a57d9dbc7962ed8233
Nava-Neeth-10 <47468411+Nava-Neeth-10@users.noreply.github.com>
2019-02-09 16:19:50 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+    int a,i=1,b=0;
+
+    scanf("%d",&a);
+
+    while(i<=a){
+
+    	b=b+i;
+
+    	i=i+1;
+
+    }
+
+    printf("%d",b);
+
+return 0;
+
+}
