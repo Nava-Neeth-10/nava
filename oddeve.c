5a312e26594ab35ec6229eafd4ccaa5ec7302a9c
Nava-Neeth-10 <47468411+Nava-Neeth-10@users.noreply.github.com>
2019-02-09 15:37:28 +0530

    Create oddeve.c

---
+++ oddeve.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+    int n;
+
+    scanf("%d",&n);
+
+          
+    
+    if(n>=0){
+
+        if(n%2==0){
+
+        printf("EVEN");
+
+    }
+
+    else{
+
+        printf("ODD");
+
+    }
+
+    }
+    else{
+printf("INVALID");
+}
+
+	return 0;
+
+}
